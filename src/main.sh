b1aa556c71105af047158c74f4d51b7d9dbb4210
Sam Sweeney <sam@samwhat.com>
2015-09-05 07:59:26 +1000

    Improved on usage

--- src/main.sh
+++ src/main.sh
@@ -12,25 +12,26 @@ function main()
 	# get task name and remove from $@
 	local task=${1:-"help"}; shift
 
-	# allow usage of on command preffix
-	local on=false
-	[ "$1" = "on" ] && shift && on=true
 
 	# find servers to task upon
 	local manifest
 	local servers
 
+	# allow usage of on command preffix
+	local on=false
+	[ "$1" = "on" ] && shift && on=true
+
 	# manifest is included in params
 	$on && manifest=${1:-""} && shift
 
-	# try use default manifest if none was provided
-	_is_empty $manifest && manifest=${TASKS_MANIFEST:-"$TASKS_PATH/manifest"}
-
 	# load manifest file into an array
-	_is_file $manifest && read -a servers <<< $(cat $manifest)
+	$on && _is_file $manifest && read -a servers <<< $(cat $manifest)
 
 	# no manifest was found, so try it as a hostname instead (only valid after on preffix)
 	$on && _is_empty $servers && _is_hostname $manifest && servers=$manifest
+
+	# no manifest provided, use default manifest instead
+	[ $on = false ] &&  _is_empty $servers && _is_file ${TASKS_MANIFEST:-"$TASKS_PATH/manifest"} && read -a servers <<< $(cat ${TASKS_MANIFEST:-"$TASKS_PATH/manifest"})
 
 	# no servers found
 	_is_empty $servers && echo "${red}ERROR${reset} You're missing a manifest file. See the readme for help." && exit 1
@@ -40,7 +41,7 @@ function main()
 	_is_not_array servers && runner[0]=$servers
 
 	# load task
-	_load_task $task "${runner[@]}" "$@"
+	_load_task $task "$@"
 
 	# run task
 	local background=${TASKS_BACKGROUND:-false}
