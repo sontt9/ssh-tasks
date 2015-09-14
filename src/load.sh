aa2eacc0cd7537f1aa5080ef364d98aa730771de
Sam Sweeney <sam@samwhat.com>
2015-09-14 13:18:15 +1000

    Paths changed because of new bin directory

--- src/load.sh
+++ src/load.sh
@@ -1,11 +1,11 @@
 function _load() {
 	local task=$1; shift
 
-	local userdefined="$TASKS_SRC/src/tasks/$task.sh"
+	local userdefined="$TASKS_PATH/tasks/$task.sh"
 	_is_file $userdefined && source $userdefined && return 1
 
-	local predefined="$TASKS_SRC/tasks/$task.sh"
+	local predefined="$TASKS_SRC/src/tasks/$task.sh"
 	_is_file $predefined && source $predefined && return 1
 
-	echo "${red}No such task exists: $task${reset}" && exit 1
+	echo "${red}No such task exists: $task${reset}" && echo "searched in $userdefined and $predefined" && exit 1
 }
