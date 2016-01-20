6b21d89d81353b86d5ac69510a339f5cb5de113a
Sam Sweeney <sam@samwhat.com>
2016-01-21 00:30:06 +1000

    Help task updated

--- src/tasks/help.sh
+++ src/tasks/help.sh
@@ -1,13 +1,15 @@
 echo ""
 echo "${blue}  > ${green}SSH TASKS${reset}"
-echo "  ssh      send a command to a server"
-echo "  help     you're looking at it"
+echo ""
+echo "    ssh      send a command to a server"
+echo "    help     you're looking at it"
 echo ""
 echo "${blue}  > ${green}YOUR TASKS${reset}"
+echo ""
 for t in $TASKS_PATH/tasks/*.sh; do
 	t=$(basename $t)
 	t=${t%.*}
-	echo "  $t"
+	echo "    $t"
 done
 echo ""
 exit # required to stop task runner
